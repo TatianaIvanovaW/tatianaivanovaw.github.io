7591029774817ebc315ff1b40c9302eef7ce09f1
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-10-21 22:21:03 +0200

    add onclick handler

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
-// document.querySelector('h3').onclick = function()
+const aboutMe = document.getElementById('aboutme');
+const expirience = document.getElementById('expirience');
+const skills = document.getElementById('skills');
+
+aboutMe.addEventListener('click', openSection);
+expirience.addEventListener('click', openSection);
+skills.addEventListener('click', openSection);
+
+
+function openSection (checkId, text) {
+
+
+  aboutMe.addEventListener('click', checkIdAgain);
+  expirience.addEventListener('click', checkIdAgain);
+  skills.addEventListener('click', checkIdAgain);
+
+     function checkIdAgain(event) { 
+     return event.target.id;
+     }
+
+  checkId = checkIdAgain(checkId);
+
+      if (checkId == "aboutme") {
+        text = "block 1";
+      } else if (checkId == "expirience") {
+        text = "block 2";
+      } else if (checkId == "skills") {
+        text = "block 3";  
+      } 
+   console.log(text);
+   
+}
