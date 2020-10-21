7ddc27f396a25d87270c79071d79d86f7cae5db7
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-10-21 23:20:27 +0200

    add section event (show and hide)

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,8 @@ aboutMe.addEventListener('click', openSection);
 expirience.addEventListener('click', openSection);
 skills.addEventListener('click', openSection);
 
-
 function openSection (checkId, text) {
-
-
+    
   aboutMe.addEventListener('click', checkIdAgain);
   expirience.addEventListener('click', checkIdAgain);
   skills.addEventListener('click', checkIdAgain);
@@ -17,16 +15,37 @@ function openSection (checkId, text) {
      function checkIdAgain(event) { 
      return event.target.id;
      }
+     checkId = checkIdAgain(checkId);
 
-  checkId = checkIdAgain(checkId);
+     const style = getComputedStyle(document.getElementById("efam"));
+     const display = style.display;
+     const style1 = getComputedStyle(document.getElementById("efex"));
+     const display1 = style1.display;
+     const style2 = getComputedStyle(document.getElementById("efsk"));
+     const display2 = style2.display;
+        //  console.log(display);
+     
 
       if (checkId == "aboutme") {
-        text = "block 1";
+          if (display === "none"){
+        document.getElementById("efam").style.display = "block";
+    }else {
+        document.getElementById("efam").style.display = "none";
+    }
       } else if (checkId == "expirience") {
-        text = "block 2";
+        if (display1 === "none"){
+        document.getElementById("efex").style.display = "block";
+    }else {
+        document.getElementById("efex").style.display = "none";
+    }
       } else if (checkId == "skills") {
-        text = "block 3";  
+        if (display2 === "none"){
+            document.getElementById("efsk").style.display = "block";
+        }else {
+            document.getElementById("efsk").style.display = "none";
+        }
       } 
    console.log(text);
    
 }
+
